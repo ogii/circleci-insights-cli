0ba0bab4cdc5835722e7e08162dcf0a4a4ca9edb
Aaron Clark <aaron.clark@circleci.com>
2023-04-06 14:21:06 +0900

    Add structs to project summary command file

--- cmd/getProjectSummaryMetrics.go
+++ cmd/getProjectSummaryMetrics.go
@@ -10,6 +10,35 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type InsightsSummary struct {
+	Items Items  `json:"items"`
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+}
+
+type Items []struct {
+	ID          string     `json:"id"`
+	Name        string     `json:"name"`
+	WindowStart string     `json:"window_start"`
+	WindowEnd   string     `json:"window_end"`
+	Repository  Repository `json:"repository"`
+	Metrics     Metrics    `json:"metrics"`
+}
+
+type Repository struct {
+	VcsType   string `json:"vcs_type"`
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+}
+
+type Metrics struct {
+	SuccessRate    float64 `json:"success_rate"`
+	TotalRuns      int     `json:"total_runs"`
+	FailedRuns     int     `json:"failed_runs"`
+	SuccessfulRuns int     `json:"successful_runs"`
+	TotalCredits   int     `json:"total_credits_used"`
+}
+
 // getProjectSummaryMetricsCmd represents the getProjectSummaryMetrics command
 var getProjectSummaryMetricsCmd = &cobra.Command{
 	Use:   "getProjectSummaryMetrics",
