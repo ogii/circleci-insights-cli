ba50f65447b13a254bf0e190eec4f7bd1a3339b3
Aaron Clark <aaron.clark@circleci.com>
2023-04-06 14:23:13 +0900

    update project summary command description

--- cmd/getProjectSummaryMetrics.go
+++ cmd/getProjectSummaryMetrics.go
@@ -42,28 +42,14 @@ type Metrics struct {
 // getProjectSummaryMetricsCmd represents the getProjectSummaryMetrics command
 var getProjectSummaryMetricsCmd = &cobra.Command{
 	Use:   "getProjectSummaryMetrics",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Get summary metrics for a project's workflows",
+	Long:  `Get summary metrics for a project's workflows. Workflow runs going back at most 90 days are included in the aggregation window.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("getProjectSummaryMetrics called")
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getProjectSummaryMetricsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// getProjectSummaryMetricsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// getProjectSummaryMetricsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
