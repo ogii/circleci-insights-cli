3ad10cd804d708314053609bae04d56903d104a9
Aaron Clark <aaron.clark@circleci.com>
2023-04-13 10:49:05 +0900

    Abstract list/table printing functions to another module

--- cmd/getProjectSummaryMetrics.go
+++ cmd/getProjectSummaryMetrics.go
@@ -5,12 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
-	"github.com/jedib0t/go-pretty/list"
-	"github.com/jedib0t/go-pretty/table"
 	"github.com/ogii/circleci-insights-cli/data"
+	"github.com/ogii/circleci-insights-cli/insights"
 	"github.com/spf13/cobra"
 )
 
@@ -31,9 +29,9 @@ var getProjectSummaryMetricsCmd = &cobra.Command{
 
 		switch formatType := format; formatType {
 		case "table":
-			printInsightsSummaryTable(*insightsSummary)
+			insights.PrintInsightsSummaryTable(*insightsSummary)
 		default:
-			printInsightsSummaryList(*insightsSummary)
+			insights.PrintInsightsSummaryList(*insightsSummary)
 		}
 
 	},
@@ -73,42 +71,6 @@ func fetchInsightsSummary(slug, branch, reportingWindow string) (*data.InsightsS
 		return nil, fmt.Errorf("error unmarshaling JSON response: %v", err)
 	}
 	return &insightsSummary, nil
-}
-
-func printInsightsSummaryList(insights data.InsightsSummary) {
-	if len(insights.Items) > 0 {
-		l := list.NewWriter()
-		for _, item := range insights.Items {
-			l.AppendItem("-----------------------------")
-			l.AppendItem(fmt.Sprintf("Workflow Name: %s", item.Name))
-			l.AppendItem(fmt.Sprintf("Credits Consumed: %d", item.Metrics.TotalCredits))
-			l.AppendItem(fmt.Sprintf("Success Rate: %.2f%%", item.Metrics.SuccessRate*100))
-			l.AppendItem(fmt.Sprintf("Total Runs: %d", item.Metrics.TotalRuns))
-			l.AppendItem(fmt.Sprintf("Failed Runs: %d", item.Metrics.FailedRuns))
-			l.AppendItem(fmt.Sprintf("Successful Runs: %d", item.Metrics.SuccessfulRuns))
-		}
-		fmt.Println(l.Render())
-	} else {
-		fmt.Println("No data available.")
-	}
-}
-
-func printInsightsSummaryTable(insights data.InsightsSummary) {
-	itemsCount := len(insights.Items)
-	if itemsCount == 0 {
-		fmt.Println("No data available.")
-		return
-	}
-
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"Workflow", "Credits Consumed", "Successful Runs", "Failed Runs", "Success Rate"})
-
-	for _, item := range insights.Items {
-		t.AppendRow(table.Row{item.Name, item.Metrics.TotalCredits, item.Metrics.SuccessfulRuns, item.Metrics.FailedRuns, item.Metrics.SuccessRate * 100})
-	}
-
-	t.Render()
 }
 
 func init() {
---
+++ insights/insights.go
@@ -0,0 +1,46 @@
+package insights
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jedib0t/go-pretty/list"
+	"github.com/jedib0t/go-pretty/table"
+	"github.com/ogii/circleci-insights-cli/data"
+)
+
+func PrintInsightsSummaryList(insights data.InsightsSummary) {
+	if len(insights.Items) > 0 {
+		l := list.NewWriter()
+		for _, item := range insights.Items {
+			l.AppendItem("-----------------------------")
+			l.AppendItem(fmt.Sprintf("Workflow Name: %s", item.Name))
+			l.AppendItem(fmt.Sprintf("Credits Consumed: %d", item.Metrics.TotalCredits))
+			l.AppendItem(fmt.Sprintf("Success Rate: %.2f%%", item.Metrics.SuccessRate*100))
+			l.AppendItem(fmt.Sprintf("Total Runs: %d", item.Metrics.TotalRuns))
+			l.AppendItem(fmt.Sprintf("Failed Runs: %d", item.Metrics.FailedRuns))
+			l.AppendItem(fmt.Sprintf("Successful Runs: %d", item.Metrics.SuccessfulRuns))
+		}
+		fmt.Println(l.Render())
+	} else {
+		fmt.Println("No data available.")
+	}
+}
+
+func PrintInsightsSummaryTable(insights data.InsightsSummary) {
+	itemsCount := len(insights.Items)
+	if itemsCount == 0 {
+		fmt.Println("No data available.")
+		return
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Workflow", "Credits Consumed", "Successful Runs", "Failed Runs", "Success Rate"})
+
+	for _, item := range insights.Items {
+		t.AppendRow(table.Row{item.Name, item.Metrics.TotalCredits, item.Metrics.SuccessfulRuns, item.Metrics.FailedRuns, item.Metrics.SuccessRate * 100})
+	}
+
+	t.Render()
+}
