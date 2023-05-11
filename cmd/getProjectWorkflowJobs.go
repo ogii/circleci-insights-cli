3ecde8fc508fb5af753e70b64e697fbf4de32223
Aaron Clark <aaron.clark@circleci.com>
2023-05-11 15:37:03 +0900

    Add command to get workflow job data, and re-factor command code

--- cmd/getProjectWorkflowJobs.go
+++ cmd/getProjectWorkflowJobs.go
@@ -2,20 +2,45 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/ogii/circleci-insights-cli/insights"
 	"github.com/spf13/cobra"
 )
 
-// getProjectWorkflowJobsCmd represents the getProjectWorkflowJobs command
 var getProjectWorkflowJobsCmd = &cobra.Command{
 	Use:   "getProjectWorkflowJobs",
 	Short: "Get summary metrics for a project workflow's jobs.",
 	Long:  `Get summary metrics for a project workflow's jobs. Job runs going back at most 90 days are included in the aggregation window.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("getProjectWorkflowJobs called")
+		slug, _ := cmd.Flags().GetString("slug")
+		branch, _ := cmd.Flags().GetString("branch")
+		format, _ := cmd.Flags().GetString("format")
+		reportingWindow, _ := cmd.Flags().GetString("reporting-window")
+		workflow, _ := cmd.Flags().GetString("workflow")
+		url := fmt.Sprintf("%s/jobs", workflow)
+
+		insightsSummary, err := insights.FetchInsightsSummary(baseURL, token, slug, url, branch, reportingWindow)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch formatType := format; formatType {
+		case "table":
+			insights.PrintInsightsSummaryTable(*insightsSummary, "job")
+		default:
+			insights.PrintInsightsSummaryList(*insightsSummary, "job")
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getProjectWorkflowJobsCmd)
+	getProjectWorkflowJobsCmd.Flags().String("slug", "", "The slug for a CircleCI project in the format `gh/orgname/projectname`")
+	getProjectWorkflowJobsCmd.Flags().String("branch", "main", "The branch for a CircleCI project")
+	getProjectWorkflowJobsCmd.Flags().String("format", "table", "The format of the results to be shown")
+	getProjectWorkflowJobsCmd.Flags().String("reporting-window", "last-90-days", "The time window used to calculate summary metrics.")
+	getProjectWorkflowJobsCmd.Flags().String("workflow", "", "The name of the workflow")
+	getProjectWorkflowJobsCmd.MarkFlagRequired("slug")
+	getProjectWorkflowJobsCmd.MarkFlagRequired("workflow")
 }
--- insights/insights.go
+++ insights/insights.go
@@ -1,22 +1,25 @@
 package insights
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/jedib0t/go-pretty/list"
 	"github.com/jedib0t/go-pretty/table"
 	"github.com/ogii/circleci-insights-cli/data"
 )
 
-func PrintInsightsSummaryList(insights data.InsightsSummary) {
+func PrintInsightsSummaryList(insights data.InsightsSummary, dataType string) {
 	if len(insights.Workflows) > 0 {
 		l := list.NewWriter()
 		for _, item := range insights.Workflows {
 			l.AppendItem("-----------------------------")
-			l.AppendItem(fmt.Sprintf("Workflow Name: %s", item.Name))
+			l.AppendItem(fmt.Sprintf("%sName: %s", dataType, item.Name))
 			l.AppendItem(fmt.Sprintf("Credits Consumed: %d", item.Metrics.TotalCredits))
-			l.AppendItem(fmt.Sprintf("Success Rate: %.2f%%", item.Metrics.SuccessRate*100))
+			l.AppendItem(fmt.Sprintf("Success Rate: %.3f%%", item.Metrics.SuccessRate*100))
 			l.AppendItem(fmt.Sprintf("Total Runs: %d", item.Metrics.TotalRuns))
 			l.AppendItem(fmt.Sprintf("Failed Runs: %d", item.Metrics.FailedRuns))
 			l.AppendItem(fmt.Sprintf("Successful Runs: %d", item.Metrics.SuccessfulRuns))
@@ -27,7 +30,7 @@ func PrintInsightsSummaryList(insights data.InsightsSummary) {
 	}
 }
 
-func PrintInsightsSummaryTable(insights data.InsightsSummary) {
+func PrintInsightsSummaryTable(insights data.InsightsSummary, dataType string) {
 	itemsCount := len(insights.Workflows)
 	if itemsCount == 0 {
 		fmt.Println("No data available.")
@@ -36,12 +39,74 @@ func PrintInsightsSummaryTable(insights data.InsightsSummary) {
 
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"Workflow", "Credits Consumed", "Successful Runs", "Failed Runs", "Success Rate"})
+
+	if dataType == "workflow" {
+		t.SetTitle("Workflows")
+	} else if dataType == "job" {
+		t.SetTitle("Jobs")
+	}
+
+	t.AppendHeader(table.Row{"Name", "Credits Consumed", "Successful Runs", "Failed Runs", "Success Rate"})
 
 	for _, item := range insights.Workflows {
-		t.AppendRow(table.Row{item.Name, item.Metrics.TotalCredits, item.Metrics.SuccessfulRuns, item.Metrics.FailedRuns, item.Metrics.SuccessRate * 100})
+		t.AppendRow(table.Row{item.Name, item.Metrics.TotalCredits, item.Metrics.SuccessfulRuns, item.Metrics.FailedRuns, fmt.Sprintf("%.3f%%", item.Metrics.SuccessRate*100)})
 	}
 	t.SetStyle(table.StyleBold)
 	t.Style().Options.SeparateRows = true
 	t.Render()
+}
+
+func FetchInsightsSummary(baseURL, token, slug, url, branch, reportingWindow string) (*data.InsightsSummary, error) {
+	var insightsSummary data.InsightsSummary
+	var nextPageToken string
+
+	var client = &http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: 5,
+		},
+	}
+
+	for {
+		url := fmt.Sprintf("%s/insights/%s/workflows/%s?branch=%s&reporting-window=%s", baseURL, slug, url, branch, reportingWindow)
+		fmt.Println("View in browser: " + url)
+		if nextPageToken != "" {
+			url += "&page-token=" + nextPageToken
+		}
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return nil, fmt.Errorf("error creating HTTP request for URL %s: %v", url, err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Circle-Token", token)
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return nil, fmt.Errorf("error making HTTP request to URL %s: %v", url, err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("non-200 response from CircleCI API for URL %s: %s", url, resp.Status)
+		}
+
+		var currentPage data.InsightsSummary
+		err = json.NewDecoder(resp.Body).Decode(&currentPage)
+		if err != nil {
+			resp.Body.Close()
+			return nil, fmt.Errorf("error unmarshaling JSON response from URL %s: %v", url, err)
+		}
+
+		insightsSummary.Workflows = append(insightsSummary.Workflows, currentPage.Workflows...)
+
+		if currentPage.NextPageToken == "" {
+			break
+		}
+
+		nextPageToken = currentPage.NextPageToken
+	}
+
+	return &insightsSummary, nil
 }
